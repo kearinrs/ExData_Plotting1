fafd2ed2765a0f67c11ba902c03b332a68e3e152
kearinrs <75562460+kearinrs@users.noreply.github.com>
2021-05-25 22:09:01 +0100

    Add files via upload

---
+++ plot4.R
@@ -0,0 +1,33 @@
+#Plot 4
+
+# Reading, naming and subsetting power consumption data
+table <- "household_power_consumption.txt"
+power <- read.table(table, header = TRUE,sep = ";", na.strings = "?", dec = ".", stringsAsFactors = FALSE)
+names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+dates <- power[power$Date %in% c("1/2/2007","2/2/2007"),]
+datetime <- strptime(paste(dates$Date, dates$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+
+# initiating a composite plot with many graphs
+active <- as.numeric(dates$Global_active_power)
+reactive <- as.numeric(dates$Global_reactive_power)
+voltage <- as.numeric(dates$Voltage)
+sub1 <- as.numeric(dates$Sub_metering_1)
+sub2 <- as.numeric(dates$Sub_metering_2)
+sub3 <- as.numeric(dates$Sub_metering_3)
+
+
+# calling the basic plot function that calls different plot functions to build the 4 plots that form the graph
+png("plot4.png", width=500, height=500)
+par(mfrow=c(2,2))
+
+plot(datetime, active,type="l",  xlab="",ylab="Global Active Power")  
+plot(datetime, voltage, type="l", xlab="datetime", ylab="Voltage")
+plot(datetime, sub1, type="n", xlab="", ylab="Energy sub metering")
+lines(datetime, sub2, type="l", col="red")
+lines(datetime, sub3, type="l", col="blue")
+legend("topright", lty=, lwd = 2.5, col=c("black","red","blue"),c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),cex=-0.6)
+plot(datetime, reactive, type="l", xlab="datetime", ylab="Global_reactive_power")
+
+  
+dev.off()
+
